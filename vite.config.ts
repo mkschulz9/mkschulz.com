1b558795a123bc914a6a85e5cf575e8b6d056475
Matthew Schulz <74387737+mkschulz9@users.noreply.github.com>
2024-06-05 18:31:05 -0700

    ci: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/mkschulz9.github.io/',
+  base: '/',
   plugins: [react()],
   build: {
     outDir: 'build',
