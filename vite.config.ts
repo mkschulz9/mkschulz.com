2337515e4a1b2189b186043417ab7db521675e1b
Matthew Schulz <74387737+mkschulz9@users.noreply.github.com>
2024-06-05 17:33:53 -0700

    build: update config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/mkschu.github.io/',
   plugins: [react()],
+  build: {
+    outDir: 'build',
+    assetsDir: 'assets',
+    rollupOptions: {
+      // Additional Rollup build options can be specified here
+    },
+  },
 });
