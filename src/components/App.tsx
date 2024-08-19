5a2fb7864c691bd6065b41b20b05d0c7ffa3b13c
Matthew Schulz <74387737+mkschulz9@users.noreply.github.com>
2024-08-18 17:59:59 -0700

    feat: add pageview to dataLayer

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ThemeProvider, createTheme, CssBaseline } from '@mui/material';
 import { SideBar } from './side-bar/SideBar';
 import { ResponsiveBox } from './lib/ResponsiveBox';
 import { DropDowns } from './drop-downs/DropDowns';
 import { Footer } from './Footer';
 
+declare global {
+  interface Window {
+    dataLayer: any[];
+  }
+}
+
 export const App: React.FC = ({}) => {
+  useEffect(() => {
+    window.dataLayer.push({
+      event: 'pageview',
+      page: {
+        url: window.location.href,
+        title: document.title,
+      },
+    });
+  }, []);
+
   const theme = createTheme({
     breakpoints: {
       values: {
