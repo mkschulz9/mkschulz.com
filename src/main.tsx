5c76f1716dd6598d308cce731817a6c3720609fb
Matthew Schulz <74387737+mkschulz9@users.noreply.github.com>
2024-08-18 17:37:13 -0700

    refactor: update GTM management

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './components/App';
+import TagManager from 'react-gtm-module';
+
+const tagManagerArgs = {
+  gtmId: 'GTM-BCZVGYFTF4',
+};
+
+TagManager.initialize(tagManagerArgs);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
